2d13687506c01a4bad69101ba83d45add115a07b
Matt Hausmann <matthew.p.hausmann@gmail.com>
2024-04-17 18:42:47 -0500

    implementing functionality

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,15 +7,67 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [peopleObj, setPeopleObj] = useState([]);
+  const [planetsObj, setPlanetsObj] = useState([]);
+  const [peopleInPlanets, setPeopleInPlanets] = useState([]);
+
+  const planetsMap = {};
+
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    let peoplePromise = axios.get(urlPeople);
+    let planetsPromise = axios.get(urlPlanets);
+    Promise.all([peoplePromise, planetsPromise]).then((values) =>{
+
+      let peopleData = values[0].data;
+      let planetsData = values[1].data;
+      let planetsMap = {}
+      for(let i = 0; i < planetsData.length; i++) {
+        let currentPlanet = planetsData[i];
+        planetsMap[currentPlanet.id] = currentPlanet.name;
+      }
+
+      let peopleInPlanetList = []
+
+      for(let i = 0; i < peopleData.length; i++) {
+        let person = peopleData[i];
+        peopleInPlanetList.push({character: person.name, homeworld: planetsMap[person.homeworld]})
+      }
+      setPeopleInPlanets(peopleInPlanetList);
+
+    });
+    
+    axios
+      .get(urlPeople)
+      .then(result => {
+        setPeopleObj(result.data);
+      })
+      .catch(err => console.log(err));
+      
+    axios
+      .get(urlPlanets)
+      .then(result => {
+        for(let i = 0; i < result.data.length; i++) {
+          planetsMap[result.data[i].id] = result.data[i].name;
+        }
+        setPlanetsObj(result.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+        peopleInPlanets.map(function(obj) { return (<Character key={obj.person} character={obj.character} planet={obj.homeworld}/> )} ) 
+      }
     </div>
-  )
+  );
 }
+
 
 export default App
 
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character(personInPlanet) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [renderingHomeworld, setRenderingHomeworld] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  let handleClick = () => {setRenderingHomeworld(!renderingHomeworld);}
+
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className = "character-card" onClick = {handleClick}>
+      <h3 className = "character-name">{personInPlanet.character}</h3>
+      {renderingHomeworld && <p>Planet: <span className="character-planet"> {personInPlanet.planet} </span></p>}
     </div>
   )
 }
